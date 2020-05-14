4d46d9e506fc5d934af2c21aa369679eef324b1a
tuzovskyaf <46595929+tuzovskyaf@users.noreply.github.com>
2020-05-14 10:37:13 +0300

    Добавьте файлы проекта.

---
+++ GitLabApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = 2 * Math.Cos(x);
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
